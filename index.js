92494e48a8a0b8e18505693aa4697f5a8ccba13a
Stephen Parente <webmakersteve@gmail.com>
2014-12-04 18:49:41 -0500

    More tests committed

--- index.js
+++ index.js
@@ -3,4 +3,4 @@
 */
 
 var Client;
-module.exports = Client = require('./lib/client')
+module.exports = Client = require('./lib/client');
--- tests/index.js
+++ tests/index.js
@@ -10,19 +10,19 @@ var assert = require('assert')
 var nock = require('nock')
 
 var Client = require('../index');
-
+var validAPIKey = require('../key');
 
 describe('Client tests', function () {
 
+  var Blizzard;
 
   beforeEach(function (done) {
+    Blizzard = new Client(validAPIKey);
     done();
   });
 
 
   it('should make a successful request', function (done) {
-
-    var Blizzard = new Client(123);
 
     Blizzard._get('https://www.google.com')
       .then(function(data) {
@@ -38,17 +38,15 @@ describe('Client tests', function () {
 
   it('should generate the proper url', function (done) {
 
-    var Blizzard = new Client(123);
     var url = Blizzard.generateApiUrl(['this', 'that'], {hey: 'there'});
 
-    assert.equal(url, 'https://us.api.battle.net/this/that?hey=there&locale=en_US&apikey=123', "Should build a specific URL");
+    assert.equal(url, 'https://us.api.battle.net/this/that?hey=there&locale=en_US&apikey=' + validAPIKey, "Should build a specific URL");
     done();
 
   });
 
   it('should return a promise', function (done) {
 
-    var Blizzard = new Client(123);
     var promise = Blizzard._get('https://www.google.com');
 
     assert.equal(promise._isPromise, true, 'Is not a promise');
@@ -59,7 +57,6 @@ describe('Client tests', function () {
 
   it('should not alter input after being used', function (done) {
 
-    var Blizzard = new Client(123);
     var dummystring = 'heythere';
 
     var once = Blizzard._quote(dummystring);
@@ -73,16 +70,94 @@ describe('Client tests', function () {
 
   it('should add quotes to the string', function (done) {
 
-    var Blizzard = new Client(123);
     var dummystring = 'heythere';
-
     var once = Blizzard._quote(dummystring);
 
     assert.notEqual(dummystring, once, 'They do match');
+    done();
+
+  });
+
+  it('should return a new object', function (done) {
+
+    assert.notEqual(Blizzard.Games, null, "Games is null");
+    done();
+
+  });
+
+  it('should create an error object', function (done) {
+
+    var err = Blizzard.createError({});
+
+    if (!(err instanceof Error)) {
+      assert.fail(err, "Error", "is not an error");
+    }
 
     done();
 
   });
 
+
+});
+
+describe('Utility tests', function () {
+
+  var Blizzard;
+
+  beforeEach(function (done) {
+    Blizzard = new Client(validAPIKey);
+    done();
+  });
+
+  it('should format the realm properly', function (done) {
+
+    var realm = Blizzard.Utilities.formatting.normalizeRealm('Wyrmrest Accord');
+
+    assert.equal(realm, 'wyrmrest-accord', "not formatted properly");
+    done();
+
+  });
+
+  it('should create a valid error object', function (done) {
+
+    var body = {
+      code: 210,
+      type: 'forbidden',
+      detail: 'account not active'
+    }
+
+    var err = Blizzard.Utilities.error.createFromResponse({body: body});
+
+    assert.ok(err, "Returned nothing");
+    done();
+
+  });
+
+});
+
+describe('Warcraft tests', function() {
+
+  var Blizzard;
+
+  beforeEach(function (done) {
+    Blizzard = new Client(validAPIKey);
+    done();
+  });
+
+  // Achievement description
+  it('should fetch an achievement description', function (done) {
+
+    var deferred = Blizzard.Games.Warcraft.Achievements.getByID(212);
+
+    assert.doesNotThrow(function () {
+      deferred.then(function (response) {
+
+        done();
+      }).fail(function (error) {
+        done(new Error("Did not recieve a valid response"));
+      });
+    });
+
+  });
 
 });
