2caa1e16bf3c0a756784d5de73a38d4ba09f5d19
Stephen Parente <webmakersteve@gmail.com>
2014-12-03 22:53:00 -0500

    Got some placeholder and organization going on

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-var BattleNet = require('src/bootstrap.js');
+/**
+* @fileoverview Expose db client.
+*/
 
-exports BattleNet;
+var Client;
+module.exports = Client = require('./lib/client')
+
+var x = new Client(123);
+x.setLocale('en_US');
+var url = x.generateApiUrl(['wow', 'achievement', '2144']);
+console.log(url);
