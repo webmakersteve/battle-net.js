31863bfa1964be214894c13bf48c94603f5fe743
Stephen Parente <webmakersteve@gmail.com>
2016-07-28 00:04:49 -0700

    Updated samples

--- tests/warcraft-client.spec.js
+++ tests/warcraft-client.spec.js
@@ -5,11 +5,29 @@ var request = require('request');
 var WarcraftClient = require('../lib/warcraft-client');
 var nock = require('nock');
 
+// Mock data
+var mock = {
+  withGuild: require('./samples/chaosity-with-guild.json'),
+  failure: require('./samples/failure.json')
+};
+
 describe('Warcraft client', function() {
 
   // Create a request default
   var baseUrl = 'http://us.api.battle.net';
   var defaultRequest;
+
+  var scope;
+
+  before(function() {
+    scope = nock('http://us.api.battle.net')
+      .defaultReplyHeaders({
+        'X-Powered-By': 'Rails',
+        'Content-Type': 'application/json;charset=utf-8',
+        'Server': 'Apache'
+      });
+  });
+
   beforeEach(function() {
     defaultRequest = request.defaults({
       headers: {
@@ -42,11 +60,28 @@ describe('Warcraft client', function() {
   it('should connect to the correct url', function(cb) {
     var client = new WarcraftClient(defaultRequest);
 
-    nock('http://us.api.battle.net')
-      .get('/maelstrom/chaosity?locale=en_US&apikey=mockkey&fields=guild')
-      .reply(202, JSON.stringify({}));
+    scope
+      .get('/wow/character/maelstrom/chaosity?locale=en_US&apikey=mockkey&fields=guild')
+      .reply(202, JSON.stringify(mock.withGuild));
 
     client.getCharacter('maelstrom', 'chaosity', ['guild'], function(err, response) {
+      t.ifError(err);
+      t.deepEqual(response, mock.withGuild);
+      cb();
+    });
+  });
+
+  it('should fail when we get a 404', function(cb) {
+    var client = new WarcraftClient(defaultRequest);
+
+    scope
+      .get('/wow/character/maelstrom/chaosity?locale=en_US&apikey=mockkey&fields=guild')
+      .reply(404, JSON.stringify(mock.failure));
+
+    client.getCharacter('maelstrom', 'chaosity', ['guild'], function(err, response) {
+      t.ok(err);
+      t.equal(err.message, mock.failure.reason);
+      t.equal(response, undefined);
       cb();
     });
   });
