3cdc2c4aedf08f9f7b444f02d2801ef719fd1ba2
Stephen Parente <webmakersteve@gmail.com>
2016-07-28 00:54:55 -0700

    Added the rest of the methods. Need to finish the unit tests now

--- tests/warcraft-client.spec.js
+++ tests/warcraft-client.spec.js
@@ -7,15 +7,27 @@ var nock = require('nock');
 
 // Mock data
 var mock = {
-  withGuild: require('./samples/chaosity-with-guild.json'),
-  failure: require('./samples/failure.json')
+  achievements: require('./samples/wow/achievements.json'),
+  bosses: require('./samples/wow/bosses.json'),
+  character: require('./samples/wow/character.json'),
+  failure: require('./samples/wow/failure.json')
 };
 
 describe('Warcraft client', function() {
 
   // Create a request default
   var baseUrl = 'http://us.api.battle.net';
-  var defaultRequest;
+  var defaultRequest = request.defaults({
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': this._userAgent
+    },
+    baseUrl: 'http://us.api.battle.net',
+    qs: {
+      locale: 'en_US',
+      apikey: 'mockkey'
+    }
+  });
 
   var scope;
 
@@ -26,20 +38,6 @@ describe('Warcraft client', function() {
         'Content-Type': 'application/json;charset=utf-8',
         'Server': 'Apache'
       });
-  });
-
-  beforeEach(function() {
-    defaultRequest = request.defaults({
-      headers: {
-        'Content-Type': 'application/json',
-        'User-Agent': this._userAgent
-      },
-      baseUrl: 'http://us.api.battle.net',
-      qs: {
-        locale: 'en_US',
-        apikey: 'mockkey'
-      }
-    });
   });
 
   it('should be a function', function() {
@@ -61,12 +59,12 @@ describe('Warcraft client', function() {
     var client = new WarcraftClient(defaultRequest);
 
     scope
-      .get('/wow/character/maelstrom/chaosity?locale=en_US&apikey=mockkey&fields=guild')
-      .reply(202, JSON.stringify(mock.withGuild));
+      .get('/wow/character/maelstrom/chaosity?locale=en_US&apikey=mockkey')
+      .reply(202, JSON.stringify(mock.character));
 
-    client.getCharacter('maelstrom', 'chaosity', ['guild'], function(err, response) {
+    client.getCharacter('maelstrom', 'chaosity', function(err, response) {
       t.ifError(err);
-      t.deepEqual(response, mock.withGuild);
+      t.deepEqual(response, mock.character);
       cb();
     });
   });
@@ -75,15 +73,90 @@ describe('Warcraft client', function() {
     var client = new WarcraftClient(defaultRequest);
 
     scope
-      .get('/wow/character/maelstrom/chaosity?locale=en_US&apikey=mockkey&fields=guild')
+      .get('/wow/character/maelstrom/chaosity?locale=en_US&apikey=mockkey')
       .reply(404, JSON.stringify(mock.failure));
 
-    client.getCharacter('maelstrom', 'chaosity', ['guild'], function(err, response) {
+    client.getCharacter('maelstrom', 'chaosity', function(err, response) {
       t.ok(err);
       t.equal(err.message, mock.failure.reason);
       t.equal(response, undefined);
       cb();
     });
+  });
+
+  describe('getAchievement()', function() {
+    var client = new WarcraftClient(defaultRequest);
+
+    it('should throw if a non-string is given as the first parameter', function() {
+      t.throws(function() {
+        client.getAchievement('rick-roll');
+      });
+    });
+
+    it('should fail if the second parameter is a non function', function() {
+      t.throws(function() {
+        client.getAchievement(2144, 'hey');
+      });
+    });
+
+    it('should properly fetch json', function(cb) {
+      scope
+        .get('/wow/achievement/2144?locale=en_US&apikey=mockkey')
+        .reply(200, JSON.stringify(mock.achievements));
+
+      client.getAchievement(2144, function(err, response) {
+        t.ifError(err);
+        cb();
+      });
+    });
+
+    it('should properly report errors on failure', function(cb) {
+      scope
+        .get('/wow/achievement/2144?locale=en_US&apikey=mockkey')
+        .reply(400, JSON.stringify(mock.failure));
+
+      client.getAchievement(2144, function(err, response) {
+        t.ok(err);
+        t.equal(response, undefined);
+        cb();
+      });
+    });
+
+  });
+
+  // Next
+  describe('getBosses()', function() {
+    var client = new WarcraftClient(defaultRequest);
+
+    it('should fail if the first parameter is a non function', function() {
+      t.throws(function() {
+        client.getBosses('hey');
+      });
+    });
+
+    it('should properly fetch json', function(cb) {
+      scope
+        .get('/wow/boss?locale=en_US&apikey=mockkey')
+        .reply(200, JSON.stringify(mock.bosses));
+
+      client.getBosses(function(err, response) {
+        t.ifError(err);
+        cb();
+      });
+    });
+
+    it('should properly report errors on failure', function(cb) {
+      scope
+        .get('/wow/boss?locale=en_US&apikey=mockkey')
+        .reply(400, JSON.stringify(mock.failure));
+
+      client.getBosses(function(err, response) {
+        t.ok(err);
+        t.equal(response, undefined);
+        cb();
+      });
+    });
+
   });
 
 });
